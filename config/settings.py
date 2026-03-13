14dd737cc84bbefda45ac97c9fe76ddc834db737
mustansir <mustanbhanpura87@gmail.com>
2026-03-13 20:55:28 +0530

    removed superuser from settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 import dj_database_url
-from django.contrib.auth import get_user_model
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -95,11 +95,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'noreply@jobportal.com'
 
 
-if os.environ.get("CREATE_SUPERUSER") == "True":
-    User = get_user_model()
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser(
-            username="admin",
-            email="admin@example.com",
-            password="Admin@123"
-        )
