a461f2324bbb5a535dd630c1543e849b5fa6144b
mustansir <mustanbhanpura87@gmail.com>
2026-03-16 13:31:49 +0530

    employer dashboard

--- config/settings.py
+++ config/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-local-dev-key")
 
-#DEBUG = os.environ.get("DEBUG", "False") == "True"
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+#DEBUG = True
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".onrender.com"]
 
 INSTALLED_APPS = [
